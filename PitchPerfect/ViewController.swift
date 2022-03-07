9bc762066031acd95eb5c7f503c81f6af6fd6d0f
Yessenali Zhanaidar <yessenalijr@gmail.com>
2022-03-07 16:50:52 +0600

    autolayout

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("button was pressed")
+        recordingLabel.text = "Recording in progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+         print("stop button was pressed")
+    }
 }
 
