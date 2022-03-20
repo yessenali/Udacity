5fee380e7d607f3414407d31931adfc7c7966271
Yessenali Zhanaidar <yessenalijr@gmail.com>
2022-03-20 23:04:07 +0600

    new

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
